c083ae6b02759c5b542d1007bc28197bee6d7d6b
Alexander Doloz <doloz.alexander@gmail.com>
2016-01-14 20:11:19 +0300

    Initial Commit

---
+++ GlobusTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GlobusTest
+//
+//  Created by Alexander on 14.01.16.
+//  Copyright © 2016 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GlobusTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GlobusTest
+//
+//  Created by Alexander on 14.01.16.
+//  Copyright © 2016 Alexander. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
